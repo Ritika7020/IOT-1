4c56b799ea16e863a7ca510f77dad875f9dfd94c
Ritika7020 <ritikabagde11@gmail.com>
2026-05-03 23:29:12 +0530

    Add files via upload

---
+++ LEDBlink.ino
@@ -0,0 +1,11 @@
+//This is my first Programmimg
+#define my_led 7  // global Declaration,LED pin no.7
+
+void setup() {
+  pinMode(my_led,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(my_led, HIGH);
+}
+    
