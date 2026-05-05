58de38d20ce182600f793161cb46af59ed6e1e35
Ritika7020 <ritikabagde11@gmail.com>
2026-05-05 16:54:27 +0530

    Add files via upload

---
+++ Parallel.ino
@@ -0,0 +1,33 @@
+#define my_led1 8
+#define my_led2 7
+#define my_led3 4
+
+void setup()
+{
+  pinMode(my_led1, OUTPUT);
+  pinMode(my_led2, OUTPUT);
+  pinMode(my_led3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(my_led1, HIGH);
+  digitalWrite(my_led2, LOW);
+
+  digitalWrite(my_led3, LOW);
+  delay(1000);
+
+  digitalWrite(my_led1, LOW);
+  digitalWrite(my_led2, HIGH);
+  
+  digitalWrite(my_led3, LOW);
+  delay(1000);
+
+  digitalWrite(my_led1, LOW);
+  digitalWrite(my_led2, LOW);
+
+  digitalWrite(my_led3, HIGH);
+  delay(1000);
+
+
+}
